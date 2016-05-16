0d415c4b179d6921ac6dd9ea55dac75e093c06b5
Sriram Sami <frizensami@gmail.com>
2016-05-16 22:44:24 +0800

    Added ability to get and add user channels + tests

--- app/accounts/__init__.py
+++ app/accounts/__init__.py
@@ -1,11 +1,46 @@
 from .models import User
+from app.knowledgebase import Channel
 from app import db
 
 
 class AccountManager(object):
     @staticmethod
     def get_subscribed_channels(telegram_user_id):
-        pass
+        '''
+        Checks if a user with this telegram_user_id exists in the database
+        and returns their channels
+        '''
+        user = db.session.query(User).filter(
+            User.telegram_user_id == telegram_user_id).first()
+        if user is not None:
+            return user.channels
+        else:
+            return None
+
+    @staticmethod
+    def add_channel(telegram_user_id, channel_name):
+        '''
+        Checks if a user with this telegram user id exists in the database
+        and if a channel with this name exists
+        and adds the channel to the user
+
+        Returns True if the operation was done successfully
+        False if not
+        '''
+        user = db.session.query(User).filter(
+            User.telegram_user_id == telegram_user_id).first()
+        channel = db.session.query(Channel).filter(
+            Channel.name == channel_name).first()
+
+        if (user is not None) and (channel is not None):
+            user.channels.append(channel)
+            db.session.add(user)
+            db.session.commit()
+            return True
+        else:
+            return False
+
+
 
 
 class TelegramAccountManager(object):
@@ -20,15 +55,13 @@ class TelegramAccountManager(object):
                 filter_by(telegram_user_id=telegram_user_id).\
                 scalar() is None:
 
-                # user does not exist
-                new_user = User(telegram_user_id, 0)
-                # Add to database
-                db.session.add(new_user)
-                # Commit changes
-                db.session.commit()
-                # Return false to indicate we created an account
-                return False
+            # user does not exist
+            new_user = User(telegram_user_id, 0)
+            # Add to database
+            db.session.add(new_user)
+            # Commit changes
+            db.session.commit()
+            # Return false to indicate we created an account
+            return False
         else:
             return True
-
-
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,7 @@
 from flask.ext.testing import TestCase
 
 from app import app, db
+from app.accounts import User
 
 
 class BaseTestCase(TestCase):
@@ -9,6 +10,16 @@ class BaseTestCase(TestCase):
     def create_app(self):
         app.config.from_object('config.TestConfiguration')
         return app
+
+    def create_user(self):
+        # user does not exist
+        new_user = User(123, 0)
+        # Add to database
+        db.session.add(new_user)
+        # Commit changes
+        db.session.commit()
+
+        return new_user
 
     def setUp(self):
         db.create_all()
