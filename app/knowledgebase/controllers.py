1bbe8176dee1ae2844b14c6e935bdca6c9437895
Sriram Sami <frizensami@gmail.com>
2016-06-18 10:22:46 +0800

    Added modules name to search bar

--- app/knowledgebase/controllers.py
+++ app/knowledgebase/controllers.py
@@ -5,6 +5,9 @@ from flask import Blueprint, request, render_template, flash, g, session, redire
 from app import db
 from app.knowledgebase import KBManager
 
+# Get json to convert objects to JS output
+import json
+
 # Define the main blueprint for this module: knowledgebase
 mod_knowledgebase = Blueprint('knowledgebase', __name__, url_prefix='/knowledgebase')
 
@@ -13,5 +16,15 @@ mod_knowledgebase = Blueprint('knowledgebase', __name__, url_prefix='/knowledgeb
 def home():
     # Retrieve the last
     modules = KBManager.retrive_all_module_objects()
-    return render_template('knowledgebase/home.html', modules=modules)
+
+    # Convert the modules to search content
+    search_content = []
+
+    # Loop through all the modules and add them to the semantic search bar
+    for module in modules:
+        search_content.append({'title': module.name.upper()})
+
+    # convert to json
+    search_content = json.dumps(search_content)
+    return render_template('knowledgebase/home.html', modules=modules, search_content=search_content)
 
